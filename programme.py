e12fe3793fcac0dabee634eb7663d2e931c62dfd
ValDV <valentinrsr20@gmail.com>
2023-09-13 15:28:33 +0200

    commit2

--- programme.py
+++ programme.py
@@ -1,6 +1,11 @@
-Total = 0
-for i in range(31):
-    temperature = float(input("Veuillez entrer une température : "))
-    Total = Total + temperature
-Moyenne = Total/31
-print(f"La moyenne des températures est de {Moyenne}")
+Total = 0 #Initialisation de la valeur Total
+
+for i in range(31): #Répéter 31 fois
+    
+    temperature = float(input("Veuillez entrer une température : ")) #Demande à l'utilisateur de rentrer une valeur
+    
+    Total = Total + temperature #Additionne la valeur Total et la Température
+    
+Moyenne = Total/31 #Calcule la moyenne
+
+print(f"La moyenne des températures est de {Moyenne}") #Affiche le résultat de la moyenne
